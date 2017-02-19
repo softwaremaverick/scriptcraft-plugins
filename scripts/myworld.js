864742cd4fd162688d120008102b96eb205f576b
Andrew Clark <andrew@dync.co.uk>
2017-02-19 09:26:39 +0000

    Added testrails to myworld

--- scripts/myworld.js
+++ scripts/myworld.js
@@ -18,5 +18,26 @@ function torchwall(length, depth, baseblock) {
    box0(baseblock, length, 1, depth).up().box0(blocks.torch, length, 1, depth);
 }
 
+function testrails() {
+   var drone = new Drone( self );
+
+   var baseblocks = [ 
+      blocks.wool.red, 
+      blocks.wool.orange, 
+      blocks.wool.yellow, 
+      blocks.wool.lime, 
+      blocks.wool.lightblue, 
+      blocks.wool.blue, 
+      blocks.wool.purple 
+   ];
+
+   for (var i=0; i < 7; i++) {
+       rails(drone).fwd(20, baseblocks [i % baseblocks.length]).up(30).fwd(50).down(30).fwd(10);
+
+       drone.left(2);
+   }
+}
+
 exports.railride = railride;
+exports.testrails = testrails;
 exports.torchwall = torchwall;
