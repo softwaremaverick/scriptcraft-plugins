dffe59b29881695b9d0882e5c3dac8ee94951772
Andrew Clark <andrew@dync.co.uk>
2018-01-21 17:44:08 +0000

    Initial creation of underwater structures

---
+++ scripts/underwater.js
@@ -0,0 +1,20 @@
+
+function underwaterBox(width, depth) {
+    var drone = new Drone( self );
+    drone.down(depth-1)
+         .box(blocks.glass, width, 1, width)
+         .up().box0(blocks.glass_pane, width, depth - 1, width)
+         .fwd().right().box(blocks.air, width-2, depth - 1, width - 2);
+}
+
+function underwaterGlassTunnel(width, depth, distance) {
+    var drone = new Drone( self );
+    drone.box(blocks.glass, width, 1, distance)
+         .down(depth-1)
+         .box(blocks.glass, width, 1, distance)
+         .up().box0(blocks.glass_pane, width, depth - 2, distance)
+         .fwd().right().box(blocks.air, width-2, depth - 2, distance - 2);
+}
+
+exports.underwaterBox = underwaterBox;
+exports.underwaterGlassTunnel = underwaterGlassTunnel;
