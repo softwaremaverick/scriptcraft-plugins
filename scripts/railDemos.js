c87606d90ada9f293c39a360235febf7a69bc708
Andrew Clark <andrew@dync.co.uk>
2018-07-11 10:22:58 +0100

    Initial rail demos

---
+++ scripts/railDemos.js
@@ -0,0 +1,37 @@
+
+function twistyrail(width, height) {
+    var drone = new Drone( self );
+    var remainingHeight = height;
+    var myrails = rails();
+
+    for (var remainingHeight = height; remainingHeight > 0; remainingHeight -= width) {
+        myrails.fwd()
+               .up(width)
+               .turnLeft();
+    }
+}
+
+function funtower(width, height) {
+    var drone = new Drone( self );
+    var remainingHeight = height;
+
+    drone.box(blocks.oak, width, height, width)
+         .right(width+1)
+         .back();
+
+    var myrails = rails(drone);
+    myrails.fwd();
+
+    var first = true;
+
+    for (var remainingHeight = height; remainingHeight > 0; remainingHeight -= width) {
+        myrails.fwd()
+               .up(first? width : width+2)
+               .turnLeft();
+
+        first = false;
+    }
+}
+
+exports.twistyrail = twistyrail;
+exports.funtower = funtower;
